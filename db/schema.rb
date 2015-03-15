50ed49cd4938599f784b8433aaab9702da501399
vampire <winsandymyint@gmail.com>
2015-03-16 03:05:42 +0800

    Updated the App

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315151105) do
+ActiveRecord::Schema.define(version: 20150315185919) do
 
   create_table "colleges", force: :cascade do |t|
     t.string   "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150315151105) do
     t.integer  "mentor_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "sex"
   end
 
   create_table "universities", force: :cascade do |t|
