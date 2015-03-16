044998972d7deb23ca61ee8e529ea4e19683d4cd
vampire <winsandymyint@gmail.com>
2015-03-17 00:18:32 +0800

    Remove Paginate in University

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -4,7 +4,7 @@ class UniversitiesController < ApplicationController
   # GET /universities
   # GET /universities.json
   def index
-    @universities = University.all.paginate(:page => params[:page], :per_page => 30)
+    @universities = University.all
     @universities = @universities.where(name: params["name"]) if params["name"].present?
     @universities = @universities.where(tuition_fees: params["tuition_fees"]) if params["tuition_fees"].present?
   end
